changed fig plot to fig save & fixed slope calculation to shift by window size instead of by 1

pulling/calc_extension_rate.py
@@ -8,11 +8,11 @@ rad_gyr_arr = np.loadtxt('rad_gyr.dat', usecols=(1,4))
 
 num_pts = rad_gyr_arr.shape[0]
 
-window_size = 50
+window_size = 15
 
 slope_list = []
 
-for start_idx in range(num_pts-window_size):
+for start_idx in range(0,num_pts-window_size-1,window_size):
     end_idx = start_idx + window_size
     t = rad_gyr_arr[start_idx:end_idx,0]
     rg = rad_gyr_arr[start_idx:end_idx,1]
@@ -32,10 +32,12 @@ zero_t   = slope_arr[zero_idx, 0]
 
 relax_time = zero_t - slope_arr[0,0]
 
-with open('relax_time.txt', 'w') as f:
-    f.write(str(relax_time)+'\n')
+np.savetxt("contraction_rate.txt", slope_arr, fmt="%.8f", delimiter=" ")
 
-plot_bool = False
+#with open('relax_time.txt', 'w') as f:
+#    f.write(str(relax_time)+'\n')
+
+plot_bool = True
 
 if plot_bool:
 
@@ -44,6 +46,6 @@ if plot_bool:
 
     ax1.scatter(t, rg)
     ax2.plot(slope_arr[:,0], slope_arr[:,1],color='red')
-    ax2.axhline(0,color='green')
-    ax2.axvline(zero_t,color='green')
-    plt.show()
+    #ax2.axhline(0,color='green')
+    #ax2.axvline(zero_t,color='green')
+    plt.savefig("relax_time.png")