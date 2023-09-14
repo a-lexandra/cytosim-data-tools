11f4841ab13fc58db225a3cd84fc9675b5305cc7
alexandra lamtyugina <alexandral@uchicago.edu>
2023-09-14 14:33:07 -0500

    modified plotting style to match other figs in manuscript

--- pulling/calc_avg_ext_rate_10trj.py
+++ pulling/calc_avg_ext_rate_10trj.py
@@ -9,11 +9,28 @@ for trj_idx in range(10):
     data_list.append(np.loadtxt(fname))
 
 data_arr = np.array(data_list)
-plt.axhline(0,color="green",linestyle="--")
-plt.errorbar(data_arr[0,:,0], data_arr[:,:,1].mean(axis=0), yerr=data_arr[:,:,1].std(axis=0))
-plt.xlabel("t")
-plt.xlim(right=110.5)
 
-plt.ylabel("contraction rate (um/s)")
+fig, ax = plt.subplots()
+
+ax.axhline(0,color="green",linestyle="--")
+
+t = data_arr[0,:,0]
+y = data_arr[:,:,1].mean(axis=0)
+s = data_arr[:,:,1].std(axis=0)
+
+ax.plot(t, y)
+ax.fill_between(t, y-s, y+s, alpha=0.2)
+
+#plt.errorbar(data_arr[0,:,0], data_arr[:,:,1].mean(axis=0), yerr=data_arr[:,:,1].std(axis=0))
+#plt.xlabel("t")
+plt.xlim(left=109.9,right=110.5)
+
+plt.yticks(fontsize=16)
+plt.xticks(fontsize=16)
+
+fig.canvas.draw()
+ax.xaxis.set_major_locator(plt.MaxNLocator(6))
+ax.yaxis.set_major_locator(plt.MaxNLocator(6))
+#plt.ylabel("contraction rate (um/s)")
 plt.savefig("contraction_avg_10trj.png")
 
