8cc3a2d4c8a3b8777aea61921b1bd8049be7a27d
alexandra lamtyugina <alexandral@uchicago.edu>
2023-11-22 14:07:48 -0600

    fixed data type bugs

--- work_rate_density.py
+++ work_rate_density.py
@@ -16,7 +16,7 @@ class WorkRateDensity(Simulation):
 
         self.work_rate_df = pd.DataFrame(columns=['time', 'fil_id', 'work_rate'])
 
-        self.avg_cos_theta_df = pd.DataFrame(columns=['time', 'cos_theta_mean', 'cos_theta_std'])
+        self.avg_cos_theta_df = pd.DataFrame(columns=['time', 'cos_theta_mean', 'cos_theta_std', 'n_fil'])
 
     def calc_cos_theta(self):
         
@@ -24,12 +24,12 @@ class WorkRateDensity(Simulation):
             time = data_obj.time
             
             for index, row in data_obj.temp_dataframe.iterrows():
-                motor_id = row['identity']
+                motor_id = int(row['identity'])
                 
-                fil1_id = row['fiber1']
+                fil1_id = int(row['fiber1'])
                 fil1_dir = np.array([ row['dirFiber1X'], row['dirFiber1Y'] ])
 
-                fil2_id = row['fiber2']
+                fil2_id = int(row['fiber2'])
                 fil2_dir = np.array([ row['dirFiber2X'], row['dirFiber2Y'] ])
 
                 motor1_pos = np.array([ row['pos1X'], row['pos1Y'] ])
@@ -38,46 +38,59 @@ class WorkRateDensity(Simulation):
                 motor1_dir = motor2_pos - motor1_pos
                 motor2_dir = motor1_pos - motor2_pos
 
-                fil1_cos_theta = np.dot(fil1_dir, motor1_dir)/( np.sqrt(motor1_dir[0]**2 + motor1_dir[1]**2) * np.sqrt(fil1_dir[0]**2 + fil1_dir[1]**2) )
-                fil2_cos_theta = np.dot(fil2_dir, motor2_dir)/( np.sqrt(motor2_dir[0]**2 + motor2_dir[1]**2) * np.sqrt(fil2_dir[0]**2 + fil2_dir[1]**2) )
+                mag1 = np.sqrt(motor1_dir[0]**2 + motor1_dir[1]**2) * np.sqrt(fil1_dir[0]**2 + fil1_dir[1]**2) 
+                mag2 = np.sqrt(motor2_dir[0]**2 + motor2_dir[1]**2) * np.sqrt(fil2_dir[0]**2 + fil2_dir[1]**2) 
 
-                force = row['force']
+                if (mag1 > 0.0) and (mag2 > 0.0):
 
-                motor1_vel = self.unloaded_speed * (1 + force*np.dot(motor1_dir, fil1_dir)/self.unbinding_force)
-                motor2_vel = self.unloaded_speed * (1 + force*np.dot(motor2_dir, fil2_dir)/self.unbinding_force)
-                if self.simulation_df is not None:
-                    df1 = self.simulation_df.loc[ (round(self.simulation_df['time'],3) == time) & (self.simulation_df['fil_id'] == fil1_id) ]
-                    
-                    fil1_f_ext_arr = np.array( [ df1['f_dirX'].values, df1['f_dirY'].values ] )
-                    fil1_f_ext_mag = np.sqrt( fil1_f_ext_arr[0]**2 + fil1_f_ext_arr[1]**2)
-                  
-                    df2 = self.simulation_df.loc[ (round(self.simulation_df['time'],3) == time) & (self.simulation_df['fil_id'] == fil2_id) ]
+                    fil1_cos_theta = np.dot(fil1_dir, motor1_dir)/mag1
+                    fil2_cos_theta = np.dot(fil2_dir, motor2_dir)/mag2
 
-                    fil2_f_ext_arr = np.array( [ df2['f_dirX'].values, df2['f_dirY'].values ] ) 
-                    fil2_f_ext_mag = np.sqrt( fil2_f_ext_arr[0]**2 + fil2_f_ext_arr[1]**2)
-                else:
-                    fil1_f_ext_mag = 0.0
+                    force = row['force']
 
-                    fil2_f_ext_mag = 0.0
+                    motor1_vel = self.unloaded_speed * (1 + force*np.dot(motor1_dir, fil1_dir)/self.unbinding_force)
+                    motor2_vel = self.unloaded_speed * (1 + force*np.dot(motor2_dir, fil2_dir)/self.unbinding_force)
+                    if self.simulation_df is not None:
+                        df1 = self.simulation_df.loc[ (round(self.simulation_df['time'],3) == time) & (self.simulation_df['fil_id'] == fil1_id) ]
+                        if df1.shape[0]>0:
+                            fil1_f_ext_arr = np.array( [ df1['f_dirX'].values, df1['f_dirY'].values ] ).flatten()
+                            fil1_f_ext_mag = np.sqrt( fil1_f_ext_arr[0]**2 + fil1_f_ext_arr[1]**2)
+                        else: 
+                            fil1_f_ext_mag = 0.0
 
-                fil1_df = pd.DataFrame.from_dict({'time': time,
-                                        'motor_id': motor_id,
-                                        'fil_id': fil1_id,
-                                        'cos_theta': fil1_cos_theta,
-                                        'f_e': fil1_f_ext_mag,
-                                        'f_m': force,
-                                        'v_m': motor1_vel})
+                        df2 = self.simulation_df.loc[ (round(self.simulation_df['time'],3) == time) & (self.simulation_df['fil_id'] == fil2_id) ]
 
-                fil2_df = pd.DataFrame.from_dict({'time': time,
-                                        'motor_id': motor_id,
-                                        'fil_id': fil2_id,
-                                        'cos_theta': fil2_cos_theta,
-                                        'f_e': fil2_f_ext_mag,
-                                        'f_m': force,
-                                        'v_m': motor2_vel})
+                        if df2.shape[0]>0:
+                            fil2_f_ext_arr = np.array( [ df2['f_dirX'].values, df2['f_dirY'].values ] ).flatten()
+                            fil2_f_ext_mag = np.sqrt( fil2_f_ext_arr[0]**2 + fil2_f_ext_arr[1]**2)
+                        else:
+                            fil2_f_ext_mag = 0.0
+                       
+
+                    else:
+                        fil1_f_ext_mag = 0.0
+
+                        fil2_f_ext_mag = 0.0
+
+                    fil1_df = pd.DataFrame([{'time': float(time),
+                                        'motor_id': int(motor_id),
+                                        'fil_id': int(fil1_id),
+                                        'cos_theta': float(fil1_cos_theta),
+                                        'f_e': float(fil1_f_ext_mag),
+                                        'f_m': float(force),
+                                        'v_m': float(motor1_vel)}])
+
+                    fil2_df = pd.DataFrame([{'time': float(time),
+                                        'motor_id': int(motor_id),
+                                        'fil_id': int(fil2_id),
+                                        'cos_theta': float(fil2_cos_theta),
+                                        'f_e': float(fil2_f_ext_mag),
+                                        'f_m': float(force),
+                                        'v_m': float(motor2_vel)}])
                 
 
-                self.cos_theta_df = pd.concat([self.cos_theta_df, fil1_df, fil2_df], ignore_index=True)
+                    self.cos_theta_df = pd.concat([self.cos_theta_df, fil1_df, fil2_df], ignore_index=True)
+        self.cos_theta_df.to_csv('cos_theta.dat', sep='\t', index=False, mode='w')
 
     def calc_work_rate_density_per_fil(self):
         for time, time_df in self.cos_theta_df.groupby('time'):
@@ -85,26 +98,31 @@ class WorkRateDensity(Simulation):
                 work_rate_fil = 0.0
 
                 for motor_id, motor_df in fil_df.groupby('motor_id'):
-                    f_m = motor_df['f_m'].values
-                    f_e = motor_df['f_e'].values
-                    cos_theta = motor_df['cos_theta'].values
-                    v_m = motor_df['v_m'].values
+                    f_m = motor_df['f_m'].values[0]
+                    f_e = motor_df['f_e'].values[0]
+                    cos_theta = motor_df['cos_theta'].values[0]
+                    v_m = motor_df['v_m'].values[0]
 
                     work_rate_motor = ( f_m - f_e*cos_theta) * v_m * cos_theta
 
                     work_rate_fil += work_rate_motor
                 
-                fil_df = pd.DataFrame.from_dict({'time': time, 'fil_id': fil_id, 'work_rate': work_rate_fil})
+                fil_df = pd.DataFrame([{'time': time, 'fil_id': fil_id, 'work_rate': work_rate_fil}])
                 self.work_rate_df = pd.concat([self.work_rate_df, fil_df])
 
         self.work_rate_df.to_csv('work_rate.dat', sep='\t', index=False, mode='w')
 
     def calc_avg_cos_theta(self):
         for time, time_df in self.cos_theta_df.groupby('time'):
-            ct_mean = np.mean(time_df['cos_theta'].values)
-            ct_std  = np.std(time_df['cos_theta'].values)
+            n_fil = len(pd.unique(time_df['fil_id']))
+            cos_theta_arr = time_df['cos_theta'].to_numpy(dtype=np.float64)
 
-            df = pd.DataFrame({'time': time, 'cos_theta_mean': ct_mean, 'cos_theta_std': ct_std}, index=[0])
+            #cos_theta_arr = cos_theta_arr[~np.isnan(cos_theta_arr)]
+            
+            ct_mean = np.mean(cos_theta_arr)
+            ct_std  = np.std(cos_theta_arr)
+
+            df = pd.DataFrame({'time': time, 'cos_theta_mean': ct_mean, 'cos_theta_std': ct_std, 'n_fil': n_fil}, index=[0])
 
             self.avg_cos_theta_df = pd.concat([self.avg_cos_theta_df, df])
 
